228737291479a9a6c03a18d3dac5e34a57681753
riacd <1927208797@qq.com>
2023-05-25 16:31:32 +0800

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -1,6 +1,12 @@
 import streamlit as st
+from datetime import datetime
 
 import API
 
+show_time = st.slider(
+    "When do you start?",
+    value=datetime(2020, 1, 1, 9, 30),
+    format="MM/DD/YY - hh:mm")
+st.write("当前展示时间为", show_time)
 key_words = st.text_input(label='关键词')
 st.button("开始分析", on_click=API.call_analyse_key, args=[key_words])
