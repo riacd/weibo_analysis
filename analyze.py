ecf34f0574321b429c5dc496bf9bf7e7e27c7f74
Sky <sky0722@sjtu.edu.cn>
2023-05-25 16:57:40 +0800

    话题排名跟踪

--- analyze.py
+++ analyze.py
@@ -1,2 +1,21 @@
-def trace_topic(topic):
-    
+import os
+import pandas as pd
+
+def trace_topic(topic): # 话题名完全匹配
+    res = []
+    for csv_name in os.listdir('话题跟踪/'):
+        df = pd.read_csv('话题跟踪/' + csv_name)
+        if topic in list(df['话题']):
+            res.append((str(df[df['话题']==topic]['时刻'].values[0]), int(df[df['话题']==topic]['排名']))) # (时刻, 排名)
+    return res
+
+def search_topic(topic): # 话题名部分匹配（包含关系）
+    res = []
+    for csv_name in os.listdir('话题跟踪/'):
+        df = pd.read_csv('话题跟踪/' + csv_name)
+        for i in list(df['话题']):
+            if topic in i:
+                res.append((i, str(df[df['话题']==i]['时刻'].values[0]), int(df[df['话题']==i]['排名']))) # (话题, 时刻, 排名)
+    return res
+
+print(search_topic('女生'))
