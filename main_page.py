f9a100c9a21eb9c0b9c247798af6fb86aa1e50ea
riacd <1927208797@qq.com>
2023-05-28 00:21:30 +0800

    新增话题分类页面

--- main_page.py
+++ main_page.py
@@ -34,7 +34,7 @@ def call_analyse_key(time: str, key: str):
 
 
 
-topic_analyze_tab, topic_hot_analyze_tab, topic_key_analyze_tab, topic_region_analyze_tab = st.tabs(['话题分析', '话题热度分析','话题关键词分析', '话题区域及时域分析'])
+topic_analyze_tab, topic_hot_analyze_tab, topic_key_analyze_tab, topic_region_analyze_tab, topic_classification_analyze_tab = st.tabs(['话题分析', '话题热度分析','话题关键词分析', '话题区域及时域分析', '话题分类'])
 with topic_analyze_tab:
     show_time = st.select_slider(
         "回溯时间",
@@ -44,7 +44,7 @@ with topic_analyze_tab:
     st.write("当前展示时间为", show_time_str)
     topic_words = st.text_input(label='话题', value='上海迪士尼6月23日起门票调价', key=1)
     st.button("开始分析", key=2, on_click=call_analyse_key, args=[show_time_str, topic_words])
-    st.pyplot(fig, clear_figure=False)
+    st.pyplot(fig, clear_figure=True)
 
 with topic_hot_analyze_tab:
     topic_hot_words = st.text_input(label='话题', key=3, value='上海迪士尼6月23日起门票调价')
@@ -102,7 +102,7 @@ with topic_region_analyze_tab:
     start_time, end_time = st.select_slider(
     label="时间区段",
     options=API.weibo_analyse.get_time_list(),
-    value=('2023-05-25 11-04', '2023-05-25 18-18'))
+    value=('2023-05-25 11-04', '2023-05-25 18-18'), key=101)
     topic_words = st.text_input(label='话题', value='上海迪士尼6月23日起门票调价', key=15)
     search_flag = st.checkbox(label='模糊搜索')
     region_button_clicked = st.button("开始分析", key=12)
@@ -110,6 +110,26 @@ with topic_region_analyze_tab:
         region_dict = analyze.analyze_region(start_time, end_time, topic_words, search_flag)
         print(region_dict)
         st.dataframe(region_dict)
+
+with topic_classification_analyze_tab:
+    start_time_, end_time_ = st.select_slider(
+    label="时间区段",
+    options=API.weibo_analyse.get_time_list(),
+    value=('2023-05-25 11-04', '2023-05-25 18-18'), key=102)
+    classification_button_clicked = st.button("开始分析", key=13)
+    if classification_button_clicked:
+        classification_dict = analyze.topic_classification(start_time_, end_time_)
+        sizes_ = classification_dict.values()
+        labels = ['financial news', 'International news', 'mainland China politics', 'culture', 'entertainment', 'sports', 'others']
+        plt.pie(sizes_, labels=labels, autopct='%1.1f%%',
+                shadow=False, startangle=90)  # '%1.1f'：指小数点后保留一位有效数值；'%1.2f%%'保留两位小数点，增加百分号（%）;startangle=90则从y轴正方向画起
+        plt.axis('equal')  # 该行代码使饼图长宽相等
+        plt.title('trending topic classification', fontdict={'size': 15})
+        plt.legend(loc="upper right", fontsize=10, bbox_to_anchor=(1.1, 1.05), borderaxespad=0.3)  # 添加图例
+        st.pyplot(fig, clear_figure=True)
+
+
+
 
 
 
